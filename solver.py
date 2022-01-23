4c77cd014f092a8060908fd03a6437928ae77f14
Pirateninjawizard <softwaredjinn@softwaredjinn.com>
2022-01-23 17:46:21 +0800

    removed logical errors that were found after trying solver on absurdle

--- solver.py
+++ solver.py
@@ -1,3 +1,5 @@
+alphabet = [c for c in "abcdefghijklmnopqrstuvwxyz"]
+
 def round():
     with open("words") as fp:
         words = [l.strip() for l in fp]
@@ -5,17 +7,36 @@ def round():
     word = "plant"
     while True:
         sol = input(f"{word}: ")
+        greens, yellows, blanks = list(), list(), list()
+        exists = {c:0 for c in alphabet}
         for i in range(5):
             c = word[i]
             cres = sol[i]
             if cres == "g":
-                words = [w for w in words if w[i] == c]
-                continue
-            if cres == "y":
-                words = [w for w in words if c in w]
-                continue
-            words = [w for w in words if c not in w]
+                greens.append(i)
+                exists[c] += 1
+            elif cres == "y":
+                yellows.append(i)
+                exists[c] += 1
+            else:
+                blanks.append(i)
+
+        for i in greens:
+            c = word[i]
+            words = [w for w in words if w[i]==c]
+        for i in yellows:
+            c = word[i]
+            words = [w for w in words if c in w and w[i]!=c]
+        for i in blanks:
+            c = word[i]
+            maxc = exists[c]
+            if maxc==0:
+                words = [w for w in words if c not in w]
+            else:
+                words = [w for w in words if w.count(c)==maxc]
+
         word = words[0]
+
         if len(words) == 1:
             print(f"{words[0]} is the solution")
             break
